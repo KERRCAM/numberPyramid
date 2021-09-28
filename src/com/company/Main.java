9e4b42d9da18ed9aa264dd8f6d4ea05a021edcd3
KERRCAM <kerrcameron@hotmail.com>
2021-09-28 15:43:09 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("how big would you like the pyrimid to be?: ");
+    int size = input.nextInt();
+    System.out.println(size);
+
+    for (int i = 0; i<size; i++) {
+        System.out.println(size);
+    }
+
+
+
+
+
+
+    }
+}
