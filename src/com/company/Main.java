c0576ee9a8088fc7fd66163d0485bcec14497acf
KERRCAM <kerrcameron@hotmail.com>
2021-09-28 16:12:17 +0100

    list plus for loop start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+    int[] pyrimid;
     Scanner input = new Scanner(System.in);
     System.out.println("how big would you like the pyrimid to be?: ");
     int size = input.nextInt();
+    pyrimid = new int[size*2+1];
     System.out.println(size);
 
     for (int i = 0; i<size; i++) {
         System.out.println(size);
+
+        System.out.println(pyrimid);
+
     }
 
 
